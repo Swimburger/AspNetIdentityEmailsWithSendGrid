4df34d4d670a057fe134f6f7322b98a839ca5d65
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2022-03-01 15:57:18 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+using AspNetIdentityEmailsWithSendGrid;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using AspNetIdentityEmailsWithSendGrid.Data;
+using Microsoft.AspNetCore.Identity.UI.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddRazorPages();
+
+builder.Services.AddTransient<IEmailSender, SendGridEmailSender>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
---
+++ SendGridEmailSenderUsingDi.cs
@@ -0,0 +1,48 @@
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using Microsoft.AspNetCore.Identity.UI.Services;
+
+namespace AspNetIdentityEmailsWithSendGrid;
+
+public class SendGridEmailSender : IEmailSender
+{
+    private readonly IConfiguration configuration;
+    private readonly ILogger logger;
+
+    public SendGridEmailSender(IConfiguration configuration, ILogger<SendGridEmailSender> logger)
+    {
+        this.configuration = configuration;
+        this.logger = logger;
+    }
+
+    public async Task SendEmailAsync(string toEmail, string subject, string message)
+    {
+        string sendGridApiKey = configuration["SendGridApiKey"];
+        if (string.IsNullOrEmpty(sendGridApiKey))
+        {
+            throw new Exception("The 'SendGridApiKey' is not configured");
+        }
+        
+        var client = new SendGridClient(sendGridApiKey);
+        var msg = new SendGridMessage()
+        {
+            From = new EmailAddress("nielsswimberghe@gmail.com", "[YOUR_WEBSITE_NAME]"),
+            Subject = subject,
+            PlainTextContent = message,
+            HtmlContent = message
+        };
+        msg.AddTo(new EmailAddress(toEmail));
+
+        var response = await client.SendEmailAsync(msg);
+        if (response.IsSuccessStatusCode)
+        {
+            logger.LogInformation("Email queued successfully");
+        }
+        else
+        {
+            logger.LogError("Failed send email");
+            // Adding more information related to the failed email could be helpful in debugging failure,
+            // but be careful about logging PII, as it increases the chance of leaking PII
+        }
+    }
+}
